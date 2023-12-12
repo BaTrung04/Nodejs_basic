9be6c67dc9d227dd10220393544210d5cb68efde
Ba Trung <batrung0410job@gmail.com>
2023-12-12 22:25:02 +0700

    CreatePool

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,8 +1,20 @@
+const connection = require('../config/database');
 
 const getHomepage = (req, res) => {
     //process data
     //call model
-    res.send("hello world vs ba trung & nodemon")
+    let users = [];
+
+    connection.query(
+        'SELECT  * FROM Users u',
+        function (err, results, fields) {
+            users = results;
+            console.log(">>>results:", results); // results contains rows returned by server
+
+            console.log('>>>check user:', users)
+            res.send(JSON.stringify(users))
+        }
+    );
 }
 const getABC = (req, res) => {
     res.send("check abc")

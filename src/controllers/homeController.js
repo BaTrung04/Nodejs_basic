4dd22eab1a8ee196412786cc8f2e447aea980ef9
Ba Trung <batrung0410job@gmail.com>
2023-12-13 00:06:54 +0700

    design listuser

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -10,29 +10,38 @@ const getABC = (req, res) => {
 const get123 = (req, res) => {
     res.render('sample.ejs')
 }
+const getCreatePage = (req, res) => {
+    return res.render('create.ejs')
+}
 
-const postCreateUsers = (req, res) => {
+const postCreateUsers = async (req, res) => {
     // let {email, name, city} = req.body;
     let email = req.body.email;
     let name = req.body.name;
     let city = req.body.city;
     console.log(">>check email:", email, 'name:', name, "city: ", city);
 
-    connection.query(
-        `INSERT INTO Users  (email, name, city)
-        VALUES (?, ?, ?);`,
-        [email, name, city],
-        function (err, results) {
-
-            console.log(results);
-            res.send('create user success !')
-        }
+    // connection.query(
+    //     `INSERT INTO Users  (email, name, city)
+    //     VALUES (?, ?, ?);`,
+    //     [email, name, city],
+    //     function (err, results) {
+    //         res.send('create user success !')
+    //     }
+    // );
+    //----------------
+    let [results, fields] = await connection.query(
+        `INSERT INTO Users  (email, name, city) VALUES (?, ?, ?);`, [email, name, city],
     );
+
+    console.log(">>>check results: ", results)
+    res.send('create user success !')
+
 }
 
 
 module.exports = {
-    getHomepage,
+    getHomepage, getCreatePage,
     getABC,
     get123,
     postCreateUsers
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const { getHomepage, getABC, get123, postCreateUsers } = require('../controllers/homeController')
+const { getHomepage, getABC, get123, postCreateUsers, getCreatePage } = require('../controllers/homeController')
 const router = express.Router();
 
 
 
-router.get('/', getHomepage)
-router.get('/abc', getABC)
-router.get('/123', get123)
+router.get('/', getHomepage);
+router.get('/abc', getABC);
+router.get('/123', get123);
+
+router.get('/create', getCreatePage);
 
 router.post('/create-user', postCreateUsers);
 

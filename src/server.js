56a1093e06d5f8518f32ab447dfe6c47b5a871d2
Ba Trung <batrung0410job@gmail.com>
2023-12-12 21:25:45 +0700

    tai su dung connection

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const configViewEngine = require('./config/viewEngine')
 const webRoutes = require('./routes/web')
+const connection = require('./config/database')
 
 const app = express()
 const port = process.env.PORT || 8888
@@ -13,6 +14,16 @@ configViewEngine(app);
 //khai bao route
 app.use('/', webRoutes);
 
+//test connection
+
+
+// simple query
+connection.query(
+    'SELECT  * FROM Users u',
+    function (err, results, fields) {
+        console.log(">>>results:", results); // results contains rows returned by server
+    }
+);
 
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
